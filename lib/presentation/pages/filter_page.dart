d858e390b613e71a871628b586f158b5f861c1d9
murock <alfiebpage@gmail.com>
2021-04-07 08:49:01 +0100

    Starting work on listtile layout for filter page

--- lib/presentation/pages/filter_page.dart
+++ lib/presentation/pages/filter_page.dart
@@ -12,9 +12,69 @@ class FilterPage extends StatelessWidget {
         child: Column(
           children: [
             SubTitleText2('Progression'),
-            ListView(),
+            Expanded(
+              child: Wrap(
+                children: [
+                  Card(
+                    child: InkWell(
+                      splashColor: Colors.blue.withAlpha(30),
+                      onTap: () {
+                        print('Card tapped');
+                      },
+                      child: Text(
+                        'Handstand',
+                        textWidthBasis: TextWidthBasis.longestLine,
+                      ),
+                    ),
+                  ),
+                  Card(
+                    child: Text(
+                      'Human Flag',
+                      textWidthBasis: TextWidthBasis.longestLine,
+                    ),
+                  ),
+                  Card(
+                    child: FilterItem(),
+                  ),
+                ],
+              ),
+            ),
             SubTitleText2('Muscle Group')
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class FilterItem extends StatefulWidget {
+  const FilterItem({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  _FilterItemState createState() => _FilterItemState();
+}
+
+class _FilterItemState extends State<FilterItem> {
+  bool isSelected = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return IntrinsicWidth(
+      child: Card(
+        child: ListTile(
+          title: Text(
+            'Back Lever',
+            textWidthBasis: TextWidthBasis.longestLine,
+          ),
+          onTap: () {
+            this.isSelected = !this.isSelected;
+            print(this.isSelected);
+            setState(() {});
+          },
+          selected: this.isSelected,
+          tileColor: Colors.red,
         ),
       ),
     );
