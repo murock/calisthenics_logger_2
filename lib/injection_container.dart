f15e4d202b0c84c0c38b0787c60b6620dac17f8d
murock <alfiebpage@gmail.com>
2021-04-22 17:44:44 +0100

    Continue with implementing bloc, start on dependency injection

---
+++ lib/injection_container.dart
@@ -0,0 +1,8 @@
+import 'package:get_it/get_it.dart';
+
+// sl = service locator
+final sl = GetIt.instance;
+
+Future<void> init() async {
+  //! Tracked Exercises
+}
--- lib/presentation/bloc/tracked_exercise_bloc.dart
+++ lib/presentation/bloc/tracked_exercise_bloc.dart
@@ -1,14 +1,16 @@
 import 'dart:async';
 
 import 'package:bloc/bloc.dart';
+import 'package:calisthenics_logger_2/core/error/failures.dart';
+import 'package:calisthenics_logger_2/core/util/params.dart';
 import 'package:calisthenics_logger_2/domain/entities/tracked_exercise.dart';
 import 'package:calisthenics_logger_2/domain/usecases/get_specific_tracked_exercise_on_date.dart';
+import 'package:dartz/dartz.dart';
 import 'package:equatable/equatable.dart';
 
 part 'tracked_exercise_event.dart';
 part 'tracked_exercise_state.dart';
 
-// TODO: look to see where timestamp converter is used and use it here instead?
 class TrackedExerciseBloc
     extends Bloc<TrackedExerciseEvent, TrackedExerciseState> {
   final GetSpecificTrackedExerciseOnDate getSpecificTrackedExerciseOnDate;
@@ -20,11 +22,23 @@ class TrackedExerciseBloc
   Stream<TrackedExerciseState> mapEventToState(
     TrackedExerciseEvent event,
   ) async* {
-    // TODO: implement mapEventToState
-    if(event is GetTrackedExerciseForDateAndName){
-      // TODO: Here is where to do input conversion
+    if (event is GetTrackedExerciseForDateAndName) {
       yield TrackedExerciseLoading();
-      final failureOrTrackedExercises
+      final failureOrTrackedExercises = await getSpecificTrackedExerciseOnDate(
+        Params(exerciseName: event.exerciseName, date: event.date),
+      );
+      yield* _eitherLoadedOrErrorState(failureOrTrackedExercises);
     }
+  }
+
+  Stream<TrackedExerciseState> _eitherLoadedOrErrorState(
+      Either<Failure, GroupedTrackedExercises>
+          failureOrTrackedExercises) async* {
+    yield failureOrTrackedExercises.fold(
+      (failure) =>
+          TrackedExerciseError(message: 'Error getting tracked exercises'),
+      (trackedExercises) =>
+          TrackedExerciseLoaded(trackedExercises: trackedExercises),
+    );
   }
 }
