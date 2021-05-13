79ddb6ccb7d53dfa82d6f3f308e6832615fa0c7a
murock <alfiebpage@gmail.com>
2021-05-13 14:22:28 +0100

    Move blocBuilder to just above exercise list

--- lib/presentation/pages/trainingPageWidgets/training_display.dart
+++ lib/presentation/pages/trainingPageWidgets/training_display.dart
@@ -1,11 +1,13 @@
 import 'package:calisthenics_logger_2/domain/entities/tracked_exercise.dart';
+import 'package:calisthenics_logger_2/presentation/bloc/tracked_exercise_bloc.dart';
 import 'package:calisthenics_logger_2/presentation/pages/trainingPageWidgets/add_remove_row.dart';
 import 'package:calisthenics_logger_2/presentation/pages/trainingPageWidgets/band_dropdown.dart';
 import 'package:calisthenics_logger_2/presentation/pages/trainingPageWidgets/exercise_input.dart';
 import 'package:calisthenics_logger_2/presentation/pages/trainingPageWidgets/recorded_exercise_item.dart';
-import 'package:calisthenics_logger_2/presentation/widgets/styled_Container.dart';
-import 'package:calisthenics_logger_2/presentation/widgets/styled_Scaffold.dart';
+import 'package:calisthenics_logger_2/presentation/widgets/loading_widget.dart';
+import 'package:calisthenics_logger_2/presentation/widgets/message_display.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class TrainingDisplay extends StatelessWidget {
   final TrackedExercise trackedExercise;
@@ -14,49 +16,70 @@ class TrainingDisplay extends StatelessWidget {
       : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return StyledScaffold.withSampleData(
-      title: 'Training',
-      body: StyledContainer(
-        child: new Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Center(
-              child: Text(
-                'Pull Up - Today',
-                style: Theme.of(context).textTheme.headline1,
-                //  style: TextStyle(color: Colors.red),
-              ),
+    return new Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Center(
+          child: Text(
+            'Pull Up - Today',
+            style: Theme.of(context).textTheme.headline1,
+            //  style: TextStyle(color: Colors.red),
+          ),
+        ),
+        ExerciseInput(
+          mainText: '10',
+          subText: 'Reps',
+        ),
+        ExerciseInput(
+          mainText: '20',
+          subText: 'kgs',
+        ),
+        BandDropdown(),
+        AddRemoveRow(),
+        BlocBuilder<TrackedExerciseBloc, TrackedExerciseState>(
+          builder: (context, state) {
+            if (state is TrackedExerciseEmpty) {
+              return MessageDisplay(message: 'Empty tracked exercise state');
+            } else if (state is TrackedExerciseLoading) {
+              return LoadingWidget();
+            } else if (state is TrackedExerciseLoaded) {
+              return ExerciseListView();
+            } else if (state is TrackedExerciseError) {
+              return MessageDisplay(message: state.message);
+            }
+            return Container(
+              height: MediaQuery.of(context).size.height / 3,
+              child: Placeholder(),
+            );
+          },
+        ),
+      ],
+    );
+  }
+}
+
+class ExerciseListView extends StatelessWidget {
+  const ExerciseListView({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Flexible(
+      child: Container(
+        height: 200,
+        child: ListView.separated(
+          separatorBuilder: (BuildContext context, int index) => Divider(
+            color: Colors.white,
+          ),
+          itemCount: 3,
+          itemBuilder: (context, index) => Padding(
+            padding: EdgeInsets.all(1),
+            child: RecordedExerciseItem(
+              setNum: (index + 1).toString(),
+              reps: (index + 7).toString(),
             ),
-            ExerciseInput(
-              mainText: '10',
-              subText: 'Reps',
-            ),
-            ExerciseInput(
-              mainText: '20',
-              subText: 'kgs',
-            ),
-            BandDropdown(),
-            AddRemoveRow(),
-            Flexible(
-              child: Container(
-                height: 200,
-                child: ListView.separated(
-                  separatorBuilder: (BuildContext context, int index) =>
-                      Divider(
-                        color: Colors.white,
-                      ),
-                  itemCount: 3,
-                  itemBuilder: (context, index) => Padding(
-                    padding: EdgeInsets.all(1),
-                    child: RecordedExerciseItem(
-                      setNum: (index + 1).toString(),
-                      reps: (index + 7).toString(),
-                    ),
-                  ),
-                ),
-              ),
-            )
-          ],
+          ),
         ),
       ),
     );
--- lib/presentation/pages/training_page.dart
+++ lib/presentation/pages/training_page.dart
@@ -2,8 +2,7 @@ import 'package:calisthenics_logger_2/domain/entities/tracked_exercise.dart';
 import 'package:calisthenics_logger_2/injection.dart';
 import 'package:calisthenics_logger_2/presentation/bloc/tracked_exercise_bloc.dart';
 import 'package:calisthenics_logger_2/presentation/pages/trainingPageWidgets/training_display.dart';
-import 'package:calisthenics_logger_2/presentation/widgets/loading_widget.dart';
-import 'package:calisthenics_logger_2/presentation/widgets/message_display.dart';
+import 'package:calisthenics_logger_2/presentation/widgets/styled_Container.dart';
 import 'package:calisthenics_logger_2/presentation/widgets/styled_Scaffold.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -13,7 +12,7 @@ class TrainingPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return StyledScaffold.withSampleData(
         title: 'Training',
-        body: SingleChildScrollView(
+        body: StyledContainer(
           child: buildBody(context),
         ));
   }
@@ -21,40 +20,12 @@ class TrainingPage extends StatelessWidget {
   BlocProvider<TrackedExerciseBloc> buildBody(BuildContext context) {
     return BlocProvider(
         create: (_) => getIt<TrackedExerciseBloc>(),
-        child: Center(
-          child: Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: Column(
-              children: [
-                SizedBox(height: 10),
-                // TODO: move the bloc building to just above the list widget
-                BlocBuilder<TrackedExerciseBloc, TrackedExerciseState>(
-                  builder: (context, state) {
-                    if (state is TrackedExerciseEmpty) {
-                      return MessageDisplay(
-                          message: 'Empty tracked exercise state');
-                    } else if (state is TrackedExerciseLoading) {
-                      return LoadingWidget();
-                    } else if (state is TrackedExerciseLoaded) {
-                      return TrainingDisplay(
-                        trackedExercise: new TrackedExercise(
-                            numPopulatedFields: 1,
-                            exerciseName: 'test',
-                            date: new DateTime(1993),
-                            rows: []),
-                      );
-                    } else if (state is TrackedExerciseError) {
-                      return MessageDisplay(message: state.message);
-                    }
-                    return Container(
-                      height: MediaQuery.of(context).size.height / 3,
-                      child: Placeholder(),
-                    );
-                  },
-                ),
-              ],
-            ),
-          ),
+        child: TrainingDisplay(
+          trackedExercise: new TrackedExercise(
+              numPopulatedFields: 1,
+              exerciseName: 'test',
+              date: new DateTime(1993),
+              rows: []),
         ));
   }
 }
